86206c765d47980d9b3312131c4f5b322b6eedbf
samantasubedi <samantasubedi01@gmail.com>
2026-02-02 22:40:00 +0545

    sending and receiving message

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
-
+"use client";
+import React, { useEffect, useRef } from "react";
+import { Socket } from "socket.io-client";
+import { useState } from "react";
+import { setMaxIdleHTTPParsers } from "http";
+import { io } from "socket.io-client";
+type messageType = {
+  message: string;
+  timeStamp: string;
+};
+let socket: Socket;
 const HomePage = () => {
+  const [message, setmessage] = useState<string>("");
+  const [messages, setmessages] = useState<messageType[]>([]);
+  const socketRef= useRef<Socket|null>(null)
+  const handleSend = () => {
+    if (message.trim() == "") {
+      return;
+    }
+    const messageData = {
+      message: message,
+      timeStamp: new Date().toISOString(),
+    };
+    if(!socketRef.current){return}
+    socketRef.current.emit("send_message", messageData);
+    setmessage("");
+  };
+  useEffect(() => {
+      socketRef.current = io("http://localhost:4000");
+    socketRef.current.on("receive_message", (data) => {
+      setmessages((prev) => [...prev, data]);
+    });
+    return () => {
+      socketRef.current?.disconnect();
+    };
+  }, []);
   return (
-    <div>this is the homepage</div>
-  )
-}
+    <div>
+      <span>your message</span>
+      <input
+        value={message}
+        onChange={(e) => {
+          setmessage(e.target.value);
+        }}
+      ></input>
+      <button onClick={handleSend}>send</button>
+      <div >
+        {messages.map((curr) => {
+          return (
+            <div key={curr.timeStamp}>
+              <span>
+                {curr.message} ---- {curr.timeStamp}
+              </span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
