d3e7b630bbb8e46b2389a5551de3baf963450b50
samantasubedi <samantasubedi01@gmail.com>
2026-02-10 18:07:06 +0545

    fixing roomid input ui

--- frontend/components/ui/RoomCodeUi.tsx
+++ frontend/components/ui/RoomCodeUi.tsx
@@ -2,40 +2,61 @@ import React, { Dispatch, SetStateAction, useState } from "react";
 import { Input } from "./input";
 import { Button } from "./button";
 import { Socket } from "socket.io-client";
+import { Icon } from "@iconify/react";
+
 type proptype = {
   userName: string;
   socketRef: React.RefObject<Socket | null>;
 
   setShowIdInput: Dispatch<SetStateAction<boolean>>;
-  setLoader:Dispatch<SetStateAction<boolean>>
-  loader:boolean
+  setLoader: Dispatch<SetStateAction<boolean>>;
+  loader: boolean;
 };
 const RoomCodeUi = ({
   userName,
   socketRef,
-loader,
-setLoader,
+  loader,
+  setLoader,
   setShowIdInput,
 }: proptype) => {
   const [idInput, setIdInput] = useState("");
+  const handleJoin = () => {
+    console.log(idInput, "thisis the room id");
+    socketRef.current?.emit("joinRoom", { userName, roomId: idInput.trim() });
+    setLoader(true);
+  };
   return (
-    <div>
-      <div>Enter the Room Id</div>
+    <div className=" bg-slate-200 rounded-2xl p-5">
+      <div className="font-bold text-2xl "> Room ID</div>
       <Input
+        className="text-2xl! p-2! mt-4 h-13 bg-purple-50"
         value={idInput}
         onChange={(e) => {
           setIdInput(e.target.value);
         }}
-      ></Input>
-      <Button
-        onClick={() => {
-          console.log(idInput, "thisis the room id");
-          socketRef.current?.emit("joinRoom", { userName, roomId: idInput });
-          setLoader(true)
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleJoin();
+          }
         }}
-      >
-        {loader?"Joining":"Join"}
-      </Button>
+      ></Input>
+      <div className="flex justify-center mt-4">
+        <Button
+          className="bg-purple-800 p-2! h-fit cursor-pointer hover:bg-purple-700 transition-colors ease-in-out duration-300 w-full font-bold text-xl"
+          onClick={() => {
+            handleJoin();
+          }}
+        >
+          {loader ? (
+            <div className="flex gap-3 items-center">
+              <Icon icon="eos-icons:bubble-loading" />
+              <div className="flex text-lg">Connecting...</div>
+            </div>
+          ) : (
+            "Join"
+          )}
+        </Button>{" "}
+      </div>
     </div>
   );
 };
